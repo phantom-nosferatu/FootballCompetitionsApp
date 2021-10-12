337e5449d595496e68127765b981ff80105fd5af
phantom-nosferatu <no-reply>
2021-10-12 07:43:23 +0300

    create getTables query

--- app/src/main/java/com/bignerdranch/android/footballcompetitions/data/remote/api/FootballCompetitionsApi.kt
+++ app/src/main/java/com/bignerdranch/android/footballcompetitions/data/remote/api/FootballCompetitionsApi.kt
@@ -2,14 +2,20 @@ package com.bignerdranch.android.footballcompetitions.data.remote.api
 
 import com.bignerdranch.android.footballcompetitions.data.remote.model.competition.CompetitionResponse
 import com.bignerdranch.android.footballcompetitions.data.remote.model.table.TableResponse
+import com.bignerdranch.android.footballcompetitions.utils.Constants.Companion.API_TOKEN
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Path
 
 interface FootballCompetitionsApi {
 
-    @GET("v2/competitions")
-    suspend fun getCompetitions() : Response<CompetitionResponse>
 
-   @GET("/v2/competitions/{id}/standings")
-   suspend fun getTables() : Response<TableResponse>
+    @Headers("X-Auth-Token: $API_TOKEN")
+    @GET("v2/competitions")
+    suspend fun getCompetitions(): Response<CompetitionResponse>
+
+    @Headers("X-Auth-Token: $API_TOKEN")
+    @GET("/v2/competitions/{id}/standings")
+    suspend fun getTables(@Path("id") id: Int): Response<TableResponse>
 }
