33bbf5fff7e764f177e2446bdb20a44d95289d80
phantom-nosferatu <phantom.nosferatu13@gmail.com>
2022-02-17 03:27:44 +0300

    get instance method

--- app/src/main/java/com/bignerdranch/android/footballcompetitions/data/local/db/AppDatabase.kt
+++ app/src/main/java/com/bignerdranch/android/footballcompetitions/data/local/db/AppDatabase.kt
@@ -1,6 +1,8 @@
 package com.bignerdranch.android.footballcompetitions.data.local.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.bignerdranch.android.footballcompetitions.data.local.dao.CompetitionDAO
 import com.bignerdranch.android.footballcompetitions.data.local.entity.CompetitionEntity
@@ -11,4 +13,22 @@ import com.bignerdranch.android.footballcompetitions.data.local.entity.Competiti
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun competitionDao() : CompetitionDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE : AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(context.applicationContext,
+                        AppDatabase::class.java, "db_competitions")
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
 }
